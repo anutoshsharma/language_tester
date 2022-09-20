fad31073ba800dce413d4047d0edb5cfb401a8eb
anutosh <anutosh12sharma@gmail.com>
2022-09-20 15:56:45 +0530

    initial commit

---
+++ src/main/java/example/language_tester.java
@@ -0,0 +1,13 @@
+package example;
+import org.json.simple.JSONObject;
+
+
+public class language_tester {
+    public static void main(String args[]){
+        JSONObject obj=new JSONObject();
+        obj.put("name","sonoo");
+        obj.put("age",new Integer(27));
+        obj.put("salary",new Double(600000));
+        System.out.print(obj);
+    }
+}
